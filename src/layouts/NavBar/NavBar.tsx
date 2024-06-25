959cda312ee2e5846e1bce6e53733eb2c432e932
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-06-25 01:01:45 -0500

    chore: Update NavBar layout with smaller gap for mobile view feat: Add vercel json for routes

--- src/layouts/NavBar/NavBar.tsx
+++ src/layouts/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 function NavBar() {
     return (
-        <nav className="flex bg-neutral-950 justify-center gap-20 top-0 fixed w-full text-white font-semibold p-3 border-b border-white"> 
+        <nav className="flex bg-neutral-950 justify-center gap-7 lg:gap-20 top-0 fixed w-full text-white font-semibold p-3 border-b border-white"> 
             <a className="hover:underline" href="/">Home</a>
             <a className="hover:underline" href="/about">About Us</a>
             <a className="hover:underline" href="/login">Login</a>
